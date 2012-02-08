4ae5b222bfc34f55769128cd0f7f23d76365029b
Michael Schaarschmidt <michael.schaarschmidt@urz.uni-halle.de>
2012-02-08 22:38:43 +0100

    beide Dateien in converter aufgenommen

--- convert.sh
+++ convert.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 /usr/bin/iconv --from-code=iso-8859-1 --to-code=UTF-8 data/ldap_Fak.txt > data/ldap_Fak_utf8.txt
+/usr/bin/iconv --from-code=iso-8859-1 --to-code=UTF-8 data/ldap_Klinik.txt > data/ldap_Klinik_utf8.txt
+
